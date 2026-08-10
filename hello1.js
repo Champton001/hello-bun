8c687a137a675432ada791dc86c9100d423f4734
Chase <you@example.com>
2026-08-10 17:07:47 -0500

    how i fixed yesterdays hiccup

--- hello1.js
+++ hello1.js
@@ -4,3 +4,4 @@ console.log(message);
 console.log("This JavaScript runs on MY machine.");
 console.log("Chase");
 console.log("08-10-2026");
+console.log("movving through files using the terminal was tricky Ifixed it by focusing more on it ");
