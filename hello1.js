8f73596214d1b8b0a0b3c69fd1c783905254d8b1
Chase <you@example.com>
2026-08-11 10:57:50 -0500

    chaged hello1.js message

--- hello1.js
+++ hello1.js
@@ -5,3 +5,4 @@ console.log("This JavaScript runs on MY machine.");
 console.log("Chase");
 console.log("08-10-2026");
 console.log("movving through files using the terminal was tricky Ifixed it by focusing more on it ");
+console.log("This is becoing easier");
